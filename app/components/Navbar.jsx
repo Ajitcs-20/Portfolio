29f1423286892f7fb81c629f8e338dc3e0b412eb
unknown <ajitsharma4789@gmail.com>
2024-06-29 00:41:25 +0530

    Fourth Commit

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -18,11 +18,11 @@ const navLinks = [
 ]
 export default function Navbar() {
   return (
-    <nav><div className="flex flex-wrap item-center justify-between mx-auto p-8">
-        <Link href={"/"} className="text-5xl text-white font-semibold ">
+    <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90" ><div className="flex flex-wrap item-center justify-between mx-auto px-4">
+        <Link href={"/"} className="text-2xl md:text-5xl text-white font-semibold ">
         LOGO</Link>
-        <div className="menu hidden md:block md:w-auto " ad="navbar">
-            <ul>
+        <div className="menu hidden md:block md:w-auto" ad="navbar">
+            <ul className="flex p-4 md:p-0 sm:flex-row md:space-x-8 mt-0">
                 {
                navLinks.map((link, index) => (
                 <li key={index}> 
