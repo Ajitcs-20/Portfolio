b7877292aba3670145455f392a805ca83a3e466c
unknown <ajitsharma4789@gmail.com>
2024-08-05 12:11:35 +0530

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
-
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export', // Enable static export
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/Portfolio/' : '',
+    images: {
+      unoptimized: true, // Disable image optimization for static export
+    },
+  };
+  
+  export default nextConfig;
+  
