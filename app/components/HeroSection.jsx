c83e52a19f73520838ba5d8c961b365f7d1130e8
unknown <ajitsharma4789@gmail.com>
2024-09-27 15:57:37 +0530

    try

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -94,7 +94,7 @@ const HeroSection = () => {
         >
           <div className="relative w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] transition-transform duration-300 hover:scale-105 flex justify-center items-center ">
             <Image
-              src="/Portfolio/blob/main/public/images/BlackWhite.png"
+              src="/Portfolio/public/images/BlackWhite.png"
               alt="profile picture"
               className="rounded-full object-cover"
               fill
